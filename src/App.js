49a8b54a95c1e35a137c2cb3f7f5ca5ede44bd47
Sai Chandra <saichandra004@hotmail.com>
2021-02-22 16:55:55 +0530

    Upload UI added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Upload, Button, Row, Col } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
+import "antd/dist/antd.css";
+import "./App.css";
+
+const { Header, Content, Footer } = Layout;
+
+const props = {
+  action: "//jsonplaceholder.typicode.com/posts/",
+  listType: "picture",
+  previewFile(file) {
+    console.log("Your upload file:", file);
+    // Your process logic. Here we just mock to the same file
+    return fetch("https://via.placeholder.com/150", {
+      method: "POST",
+      body: file,
+    })
+      .then((res) => {
+        debugger;
+        res.json();
+      })
+      .then(({ thumbnail }) => thumbnail);
+  },
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout className="layout">
+        <Header style={{ display: "flex" }}>
+          <div className="logo">Leaf disease detection</div>
+        </Header>
+        <Content>
+          <Row gutter={16} justify="center">
+            <Col span={6}>
+              <div className="uploadFiles">
+                <p>Upload files to detect disease in a leaf</p>
+                <Upload {...props}>
+                  <Button icon={<UploadOutlined />}>Upload</Button>
+                </Upload>
+              </div>
+            </Col>
+          </Row>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>Leaf disease detection</Footer>
+      </Layout>
     </div>
   );
 }
